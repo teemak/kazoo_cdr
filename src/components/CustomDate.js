bab75e614145bdf1ca90bf9f5e2fff7cadab30a0
teemak <bunth667@gmail.com>
2020-02-20 19:49:23 -0500

    Datepicker added

--- src/components/CustomDate.js
+++ src/components/CustomDate.js
@@ -1,15 +1,30 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Input from "@material-ui/core/Input";
 import { DateRangePicker } from 'react-date-range'
+import { addDays } from 'date-fns'
+import 'react-date-range/dist/styles.css'; 
+import 'react-date-range/dist/theme/default.css'
 
 export default class CustomDate  extends Component {
+    state = {
+        start: new Date(),
+        end: addDays(new Date(), 7),
+        key: 'selection'
+    }
     render() {
         return (
             <span className="custom-date">
                 <div className="start-date">
                     <p className="start-date-header">START DATE</p>
                     <Input placeholder="Select Date" />
-                    <DateRangePicker/>
+                    <DateRangePicker 
+                        onChange={item => console.log(item)}
+                        showSelectionPreview={true}
+                        moveRangeOnFirstSelection={false}
+                        months={2}
+                        ranges={[this.state.start, this.state.end]}
+                        direction='horizontal'
+                    />
                 </div>
                 <div className="end-date">
                     <p className="end-date-header">END DATE</p>
