d284826670dba7f2755680ca6d6fbff628d5f7b9
TeeMak <bunth667@gmail.com>
2020-03-16 13:03:03 -0400

    updated format callee function

--- src/helper/formatCallee.js
+++ src/helper/formatCallee.js
@@ -8,35 +8,52 @@ const formatCallee = (callee, to, request) => {
 
 	if (callee === undefined) {
 		const c = request.split("@")[0];
+		//console.log("WHAT IS LENGTH?", c.length);
 		if (c.length === 12) {
 			// +19544141212
 			const phoneNumber = `(${c[2]}${c[3]}${c[4]}) ${c[5]}${c[6]}${c[7]} - ${c[8]}${c[9]}${
 				c[10]
 			}${c[11]}`;
-			return phoneNumber;
-			//formattedCallee = phoneNumber;
+			//console.log("PHONE NUMBER", phoneNumber);
+			//return phoneNumber;
+			formattedCallee = phoneNumber;
 		} else if (c.length === 4) {
 			// 1001
 			const c = request.split("@")[0];
 			const phoneNumber = `EXT ${c}`;
 			formattedCallee = phoneNumber;
-			return formattedCallee;
+			//console.log("formattedCallee", formattedCallee);
+			//return formattedCallee;
 		} else if (c.length === 10) {
-			//console.log("WHAT IS CALLEE", c);
+			const d = c.split("");
+			formattedCallee = `(${d[0]}${d[1]}${d[2]}) ${d[3]}${d[4]}${d[5]} - ${d[6]}${d[7]}${
+				d[8]
+			}${d[9]}`;
+			// console.log("WHAT IS CALLEE", c);
 			// conference
-			//console.log("C:", c);
-			//const titleCase = c[0].toUpperCase + c.slice(1);
-			//return titleCase;
-			return c;
+			// console.log("C:", c);
+			// const titleCase = c[0].toUpperCase + c.slice(1);
+			// return titleCase;
+			// console.log("* C IS", formattedCallee);
+			// return formattedCallee;
 		}
 		//callee = phoneNumber;
 		//callee = to;
+		//} else if (callee === undefined) { // ????? never runs
+		//callee = "UNKNOWN CALLEE?????";
+		//return callee;
 	} else {
+		/*
+		 * Callee === name   ||
+		 * Callee === number ||
+		 * Callee === ext
+		 */
+		console.log("CALLEE EXISTS", callee);
 		//console.log("TO", to);
 		//console.log("REQUEST", request);
 		//console.log("CALLEE IS NOT UNDEFINED:", callee);
-		const isNumber = callee.split("@");
-		callee = isNumber;
+		//const isNumber = callee.split("@");
+		//callee = isNumber;
 
 		/*isNumber.map(digit => {
 			//console.log("DIGIT IS:", digit);
@@ -52,9 +69,13 @@ const formatCallee = (callee, to, request) => {
 
 		return callee;
 	}
+	//console.log("DEFAULT RETURN", to);
+	//return to;
+	//console.log("DEFAULT:", request);
+	//return request;
 	// TEMP
 	//formattedCallee = request;
-	//return formattedCallee;
+	return formattedCallee;
 };
 
 export default formatCallee;
